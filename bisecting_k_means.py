94e12ea3c95451802b994cf958a3be738ed23062
SergeySenigov <ssbox@yahoo.com>
2025-06-19 12:04:24 +0300

    bisecting kmeans

--- bisecting_k_means.py
+++ bisecting_k_means.py
@@ -2,13 +2,18 @@
 from pyspark.ml.clustering import BisectingKMeans
 from pyspark.ml.linalg import Vectors
 from pyspark.sql import SparkSession
+from pyspark import SparkConf
 
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.config("appName", "PythonBisectingKMeansExample").getOrCreate()
 
+    conf = SparkConf().setAppName('PythonBisectingKMeansExample').setMaster('local')
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
+    # Create a BisectingKMeans model with k=3
     bkm = BisectingKMeans(k=3, featuresCol = 'features', predictionCol = 'cluster')  
 
+    # Create a DataFrame with the training data
     data = [
         (Vectors.dense([0.0, 0.0]), ), (Vectors.dense([1.0, 1.0]), ),
         (Vectors.dense([1.0, 8.0]), ), (Vectors.dense([0.0, 9.0]), ),
@@ -16,22 +21,30 @@ if __name__ == "__main__":
         
     df = spark.createDataFrame(data, ["features",])
     
+    # Fit the model to the data
     model = bkm.fit(df)
+
+    # Print the cluster centers and the predictions for each data point
     print("\nCluster Centers:", model.clusterCenters())
     print("Training clusters:")
     model.summary.predictions.show(truncate=False)
     
-    data_predict = [
-        (Vectors.dense([4.0, 3.0]), ), (Vectors.dense([6.0, 7.0]), ),
-        (Vectors.dense([6.0, 4.0]), ), (Vectors.dense([9.0, 9.0]), )  ]
+    float_range = [i * 0.5 for i in range(-10, 27)]
+
+    data_predict2 = []
+    for x in float_range:
+        for y in float_range:
+            data_predict2.append((Vectors.dense([x, y]), ))
+    
+    data_predict = data_predict2
 
     df_predict = spark.createDataFrame(data_predict, ["features",])
-
-    print("New predictions:")
-    for d in data_predict:
-        predicted_cluster = model.predict(d[0])
-        print(d[0], ', predicted cluster', predicted_cluster)
     
-    print("\nNew predictions (the same with transform()):")
+    # Print the new predictions using the transform() method
+    print("\nNew predictions using with transform() method:")
     model.setPredictionCol("predicted cluster")
-    model.transform(df_predict).show(truncate=False)
+    df_predicted = model.transform(df_predict)
+
+    l = df_predicted.collect()
+    for i in l:
+        print(str(i[0][0]).replace(".", ","), ";", str(i[0][1]).replace(".", ","), ";", i[1], sep="")
