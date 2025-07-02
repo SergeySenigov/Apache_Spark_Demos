f7a871b8c53f6b04b4807686fe1aead494b35938
SergeySenigov <ssbox@yahoo.com>
2025-07-02 16:46:02 +0300

    rate_stream_continuous

--- rate_stream_continuous.py
+++ rate_stream_continuous.py
@@ -12,13 +12,18 @@ df_stream = spark.readStream \
 
 query = (df_stream\
     .filter(df_stream.value % 2 == 0)\
+    # .withColumn('mod3', df_stream.value % 3) \
     .withColumn('tag', lit('Rate stream with 10 rows per sec in continuous mode')) \
     .select('value', 'tag') \
+    # .select('value', 'mod3', 'tag') \
+    # .groupBy('mod3')\
+    # .count() \
     .writeStream \
-    .outputMode("append") \
     .option('truncate', 'False') \
     .format("console") \
+    .outputMode('update') \
     .trigger(continuous="1 second") \
+    # .trigger(processingTime='2 second') \
     .start())
 
 spark.streams.awaitAnyTermination()
